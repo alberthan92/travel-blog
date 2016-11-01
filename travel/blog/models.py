7aa3f4082fa2341b7304511abd7bcb61ae2b7dba
alberthan92 <alberthan92@gmail.com>
2016-11-01 22:16:39 +0900

    Created a model called Post for the blog.

--- travel/blog/models.py
+++ travel/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=40)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(null=True, blank=True)
+

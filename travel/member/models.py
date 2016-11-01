18e48d064429cb18bb7735b0fb53f2a893ca072a
alberthan92 <alberthan92@gmail.com>
2016-11-01 22:17:22 +0900

    Created a custom User.

--- travel/member/models.py
+++ travel/member/models.py
@@ -1,14 +1,62 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import \
+    AbstractBaseUser, \
+    BaseUserManager, \
+    PermissionsMixin
 
-class MyUser(AbstractBaseUser):
+class MyUserManager(BaseUserManager):
+    def create_user(
+            self,
+            email,
+            last_name,
+            first_name,
+            password=None,
+    ):
+        user = self.model(
+            email=email,
+            last_name=last_name,
+            first_name=first_name,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(
+            self,
+            email,
+            last_name,
+            first_name,
+            password,
+    ):
+        user = self.model(
+            email=email,
+            last_name=last_name,
+            first_name=first_name,
+        )
+        user.set_password(password)
+        user.save()
+        user.is_staff = True
+        user.is_superuser = True
+        return user
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=100, unique=True)
     last_name = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30)
     date_joined = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=False)
+
 
     USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = ['email', ]
+    REQUIRED_FIELDS = ['last_name', 'first_name', ]
+
+    objects = MyUserManager()
+
+    def get_full_name(self):
+        return '%s%s' % (self.last_name, self.first_name)
+
+    def get_short_name(self):
+        return self.first_name
 
 
 # Create your models here.
